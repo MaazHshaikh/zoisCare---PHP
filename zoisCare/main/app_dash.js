bab657e11441624030ac7347270e275b80ce6f0b
MaazHshaikh <91615848+MaazHshaikh@users.noreply.github.com>
2024-03-23 18:10:06 +0500

    Delete zoisCare/main directory

--- zoisCare/main/app_dash.js
+++
@@ -1,5 +0,0 @@
-// const addCityButton = document.getElementById("add-city-button");
-// addCityButton.addEventListener("click", function() {
-//   // define the action to be taken when the button is clicked
-//   console.log("Add city button clicked!");
-// });
